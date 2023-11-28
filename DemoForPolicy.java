d889a025ecfc57d613624535bd4236a56b812098
voldielouza <148798478+voldielouza@users.noreply.github.com>
2023-11-27 21:41:22 -0500

    Update DemoForPolicy.java
    Added code to implicitly call toString method and output information about the Policy and PolicyHolders.

--- DemoForPolicy.java
+++ DemoForPolicy.java
@@ -52,20 +52,9 @@ public class DemoForPolicy
       }
       
       
-      for (Policy policy : Policies)
+       for (Policy policy : Policies)
       {
-         
-         System.out.println("Policy Number: " + policy.getPolicyNumber());
-         System.out.println("Provider Name: " + policy.getProviderName());
-         System.out.println("Policyholder's First Name: " + policy.getFirstName());
-         System.out.println("Policyholder's Last Name: " + policy.getLastName());
-         System.out.println("Policyholder's Age: " + policy.getAge());
-         System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
-         System.out.println("Policyholder's Height: " + policy.getHeight() + " inches");
-         System.out.println("Policyholder's Weight: " + policy.getWeight() + " pounds");
-         System.out.printf("Policyholder's BMI: %.2f\n", policy.getBMI());
-         System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
-         System.out.println();
+         System.out.println(Policy.get(i));
       }
       int smokerCount = 0;
       
@@ -77,6 +66,7 @@ public class DemoForPolicy
       System.out.println("The number of policies with a smoker is; " + smokerCount);
       System.out.println("The number of policies with a non-smoker is; " + (policies.size()-smokerCount));
    }
+      
       
 }
 
